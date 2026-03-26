0cd9aff081614ed3627002f7d484141a0cb3a512
Ini Rizky <85105054+inirizky@users.noreply.github.com>
2026-03-26 15:38:02 +0700

    vercel9

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,13 +1,10 @@
 import pino from 'pino';
 
-export const logger = pino({
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname',
-        },
-    },
-    level: process.env.LOG_LEVEL || 'info',
-});
+export const logger = pino(
+    process.env.NODE_ENV === 'production'
+        ? { level: process.env.LOG_LEVEL || 'info' }
+        : {
+            transport: { target: 'pino-pretty', options: { colorize: true } },
+            level: process.env.LOG_LEVEL || 'info',
+        }
+);
